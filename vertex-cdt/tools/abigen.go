cdfdd8c8d810e0958fb674e3590303654e6a8cfe
quan <quan.dang.nguyen@quoine.com>
2019-10-21 17:54:12 +0700

    parse abi

--- vertex-cdt/tools/abigen.go
+++ vertex-cdt/tools/abigen.go
@@ -1,10 +1,36 @@
 package tools
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os/exec"
 	"strings"
 )
+
+type Parameter struct {
+	IsArray bool   `json:"is_array"`
+	Type    string `json:"type"`
+}
+type Function struct {
+	Name       string      `json:name`
+	Parameters []Parameter `json:parameters`
+}
+type ABI struct {
+	Version   string     `json:version`
+	Functions []Function `json:functions`
+}
+type Ctype struct {
+	Tag string `json:tag`
+}
+type Cparam struct {
+	Tag  string `json:tag`
+	Type Ctype  `json:type`
+}
+type CFunction struct {
+	Name       string   `json:name`
+	Parameters []Cparam `json:parameters`
+}
 
 func ABIgen(file string, language string) {
 	names := strings.Split(file, "/")
@@ -21,5 +47,29 @@ func ABIgen(file string, language string) {
 	if err != nil {
 		fmt.Println(err)
 	}
+	parse(jsonFile)
 	fmt.Print(string(out))
+}
+func parse(file string) {
+	jsonFile, _ := ioutil.ReadFile(file)
+	data := []CFunction{}
+	_ = json.Unmarshal([]byte(jsonFile), &data)
+	result := ABI{}
+	result.Version = "1.0"
+	functions := []Function{}
+	for i := 0; i < len(data); i++ {
+		params := []Parameter{}
+		fmt.Println(data[i])
+		for j := 0; j < len(data[i].Parameters); j++ {
+			param := Parameter{false, data[i].Parameters[j].Type.Tag[1:]}
+			params = append(params, param)
+		}
+		function := Function{data[i].Name, params}
+		functions = append(functions, function)
+	}
+	result.Functions = functions
+	fmt.Println(result)
+	resultJson, _ := json.Marshal(result)
+	err := ioutil.WriteFile(file, resultJson, 0644)
+	fmt.Printf("%+v", err)
 }
