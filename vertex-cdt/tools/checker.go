2831674658928565c595b0fbc3ee15ecd9517fb6
quan <quan.dang.nguyen@quoine.com>
2019-10-24 15:13:20 +0700

    add allow function

--- vertex-cdt/tools/checker.go
+++ vertex-cdt/tools/checker.go
@@ -6,7 +6,7 @@ import (
 	wasm "github.com/wasmerio/go-ext-wasm/wasmer"
 )
 
-var AllowFunctionEnv = []string{"vs_value_set", "vs_value_get", "vs_value_size_get"}
+var AllowFunctionEnv = []string{"vs_value_set", "vs_value_get", "vs_value_size_get", "chain_storage_size_get", "chain_storage_get", "chain_storage_set", "chain_print_bytes", "chain_event_emit", "chain_get_caller", "chain_get_creator", "chain_invoke"}
 var AllowImportWasi = "wasi_unstable"
 
 func checkFunction(fun string) bool {
