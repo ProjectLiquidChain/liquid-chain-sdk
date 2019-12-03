8cb625f558c6298c133e140aa453770e88cd962b
quan <quan.dang.nguyen@quoine.com>
2019-12-03 15:50:19 +0700

    change build

--- build-c.sh
+++ build-c.sh
@@ -2,6 +2,7 @@ brew install unzip
 unzip wasi-sdk-7.0.zip
 rm -rf /usr/local/opt/wasi-sdk/
 cp -r ./wasi-sdk-7.0/opt/ /usr/local/opt/
+rm -rf ./wasi-sdk-7.0
 brew install llvm@9
 cd c2ffi/
 mkdir build
@@ -11,4 +12,3 @@ make
 mv ./bin/c2ffi /usr/local/bin
 cd ../..
 cd vertex-cdt && ./build.sh
-rm -rf ../wasi-sdk-7.0
