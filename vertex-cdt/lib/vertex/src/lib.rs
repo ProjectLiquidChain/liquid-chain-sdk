0645fc8ab9b74fddca19abc70d2aa1ca0fa1d818
quan <quan.dang.nguyen@quoine.com>
2019-12-18 14:49:15 +0700

    update lib rust

--- vertex-cdt/lib/vertex/src/lib.rs
+++ vertex-cdt/lib/vertex/src/lib.rs
@@ -1,8 +1,52 @@
-pub type address =  [u8;35];
+use std::mem;
+use byteorder::{ByteOrder, LittleEndian};
+extern crate libc;
+pub type address =  *const u8;
 pub type Event = i32;
 extern {
-   pub fn chain_storage_get(x: &[u8]) ->  &[u8] ;
-   pub fn chain_get_caller() -> address ;
-   pub fn chain_get_creator() -> address;
-   pub fn chain_storage_set(x: &[u8], y: &[u8]) ;
+   fn chain_storage_size_get(key: *const u8, length: u8 ) -> usize;
+   fn chain_get_caller(caller: address) ;
+   fn chain_get_creator(creator: address);
+   fn chain_storage_get(key: *const u8, length: u8, value: *const u8) -> *const u8 ;
+   fn chain_storage_set(key: *const u8, key_size: u8, value: *const u8, value_size: u8) ;
+}
+
+pub fn get_caller() -> address {
+    let caller: address = unsafe {
+        libc::malloc((mem::size_of::<u8>() * 35) as libc::size_t ) as address
+    };
+    unsafe {
+        chain_get_caller(caller) ;
+    }
+    return caller;
+}
+pub fn get_creator() -> address {
+    let creator: address = unsafe {
+        libc::malloc((mem::size_of::<u8>() * 35) as libc::size_t ) as address
+    };
+    unsafe {
+        chain_get_creator(creator) ;
+    }
+    return creator;
+}
+pub fn storage_get(key: *const u8, key_size: u8) -> *const u8 {
+    let size : usize = unsafe { chain_storage_size_get(key, key_size ) };
+    if size == 0 {
+        return 0 as *const u8;
+    }
+    unsafe {
+        let ret : *const u8 = libc::malloc((mem::size_of::<u8>() * size) as libc::size_t ) as *const u8;
+        chain_storage_get(key, key_size, ret);
+        return ret ;
+    };
+}
+pub fn storage_set(key: *const u8, key_size: u8 , value: *const u8, value_size: u8 ) {
+    unsafe {
+        chain_storage_set(key, key_size, value, value_size ) ;
+    }
+}
+pub fn from_byte(x: *const u8, length: u8) -> u64 {
+    let key = unsafe { std::slice::from_raw_parts(x, length as usize) };
+
+    return LittleEndian::read_uint(key, length as usize).to_be();
 }
