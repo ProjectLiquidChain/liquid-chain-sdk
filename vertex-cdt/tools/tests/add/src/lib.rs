6aae913ceb486adbc7ace5893e84b6cd30443fa6
quan <quan.dang.nguyen@quoine.com>
2019-12-20 15:36:16 +0700

    fix build

--- vertex-cdt/tools/tests/add/src/lib.rs
+++ vertex-cdt/tools/tests/add/src/lib.rs
@@ -1,8 +1,14 @@
 extern crate vertex_lib;
 use vertex_lib::*;
+// declare event
 extern {
    fn Add(x: i32, y: i32) -> Event;
 }
+// declare public function
+/*
+    fn add(x: i32, y:i32)
+    return i32
+ */
 #[no_mangle]
 pub fn add(x: i32, y: i32) -> i32
 {
