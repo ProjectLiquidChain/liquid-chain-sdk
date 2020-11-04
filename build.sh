64f62f69657e029dfa1fca63de7b9b3af390f0e9
dangquan-quoine <quan.nguyen.dang@quoine.com>
2020-11-04 16:51:11 +0700

    add build linux

--- build.sh
+++ build.sh
@@ -62,7 +62,57 @@ build_mac () {
 }
 build_linux() {
     ## need support
+    if [ ! -f "/usr/local/bin/cmake" ]
+    then
+        echo "cmake not exits"
+        apt-get install cmake
+    fi
+    if [ ! -d "/usr/local/Cellar/unzip" ]
+    then
+        echo "unzip not exits"
+        apt-get install unzip
+    fi
+    # unzip and install wasi-sdk
+    unzip wasi-sdk-7.0.zip
     echo ""
+    # install llvm, require llvm version 9
+    if [ ! -d "/usr/local/opt/llvm/" ]
+    then
+        echo "llvm DOES NOT exists, install llvm: "
+        apt-get install -y llvm-9
+    fi
+    # install c2ffi tool to support generate ABI from c, c++ langiage
+    cd c2ffi/
+    mkdir build
+    cd build/
+    LLVM_DIR=/usr/local/opt/llvm/ cmake ..
+    make
+    mv ./bin/c2ffi /usr/local/bin
+    if [ ! -f "/usr/local/bin/c2ffi" ]
+    then
+        echo "c2ffi not exists, please check config in c2ffi"
+        exit
+    else
+        echo "c2ffi exists."
+        cd ../..
+        #install rust toolchain, choose option 1 to default install rust toolchain
+        if [ ! -x "$(command -v cargo)" ]
+        then
+            echo "rust toolchain not exit"
+            curl https://sh.rustup.rs -sSf | sh
+            source $HOME/.cargo/env
+            rustup target add wasm32-wasi
+            rustup update nightly
+        fi
+        # install liquid-cdt
+        cd liquid-cdt && ./build.sh
+        if [ ! -f "/usr/local/bin/liquid-cdt" ]
+        then
+            echo "build liquid-cdt fail, please check go version"
+        else
+            echo "Build successful ! "
+        fi
+    fi
 }
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
